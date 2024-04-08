d503526024396967636d77b0296afd2df4857333
Altan Avtzi <avtzialtan@hotmail.com>
2024-04-08 12:43:32 +0300

    Add build setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+  name="blockchat",
+  version="0.1.0",
+  author="Altan Avtzi",
+  author_email="avtzialtan@gmail.com",
+  description="A simple blockchain network",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/avtzis/ntua-blockchat",
+  install_requires=[
+    "cffi",
+    "cryptography",
+    "pycparser",
+    "prompt_toolkit"
+  ],
+  python_requires=">=3.11",
+  entry_points={
+    "console_scripts": [
+      "blockchat=blockchat.__main__:main"
+    ]
+  },
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.11",
+    "License :: OSI Approved :: MIT License",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+  keywords="ntua blockchain crypto distributed-systems",
+)
